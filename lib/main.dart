03491a3bbfadb27be081cb226e0372e0a29f6c4d
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 <sany.aleks2012@gmail.com>
2026-08-27 03:57:25 +0500

    Android

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:signals/signals_flutter.dart';
 import 'package:detool64/app.dart';
 import 'package:detool64/logic/item_database.dart';
+import 'package:detool64/logic/enchantments_manager.dart'; // <-- Добавлен импорт
 import 'package:detool64/themes.dart';
 
 Logs constructLogger() {
